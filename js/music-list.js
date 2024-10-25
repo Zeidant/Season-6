d00a28c1c5bf9799d46178a9886f25721a074310
Anthony Feliz <anthonyemma55@gmail.com>
2024-10-24 23:23:18 -0400

    Season 6, day 53

--- js/music-list.js
+++ js/music-list.js
@@ -311,6 +311,12 @@ const allMusic = [
     "img": "music-52",
     "src": "music-52"
   },
+  {
+    "name": "53. Writing over the sign",
+    "artist": "\u660e\u661f <Akeboshi>",
+    "img": "music-53",
+    "src": "music-53"
+  },
 ];
 
 export default allMusic;
